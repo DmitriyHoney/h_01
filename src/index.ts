a659f85e2eff37c38b120cb5678cfe80c568e54d
Bolshakov Dmitry <vanove2013@yandex.ru>
2023-02-02 21:36:17 +0300

    add root route

--- src/index.ts
+++ src/index.ts
@@ -17,4 +17,5 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/v1/products', productsRoute);
 
+app.get('/', (req, res) => res.send('Hello, world!'));
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
